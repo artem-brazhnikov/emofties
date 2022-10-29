c838aaadd557939c4a9f715cf6bcf9f49a4da398
Artem B <artem.bv@pm.me>
2022-10-29 10:28:11 +0100

    chore: setup env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,56 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-toolbox"
+import "dotenv/config"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
+import "hardhat-deploy"
+import "@typechain/hardhat"
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli/ex"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
-export default config;
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [{ version: "0.8.17" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+      saveDeployments: true,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
+  },
+}
