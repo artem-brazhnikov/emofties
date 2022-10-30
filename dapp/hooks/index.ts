0d08874914dfdbca48c9ada9dd613d79491e680f
Artem B <artem.bv@pm.me>
2022-10-30 08:07:47 +0000

    chore: update contract addr

--- dapp/hooks/index.ts
+++ dapp/hooks/index.ts
@@ -5,7 +5,7 @@ import { useNetwork } from "@web3modal/react"
 //   "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 export const emoftiesProtocolAddress =
-  "0x2886E531083133F011f4545B5eD3fa5aC65b9371"
+  "0x2dBa017c6A9c9e6302d973E93Eba7491A8D389f8"
 export function useCurrentChainId(): number | undefined {
   const { network, isReady } = useNetwork()
   return network?.chain?.id
