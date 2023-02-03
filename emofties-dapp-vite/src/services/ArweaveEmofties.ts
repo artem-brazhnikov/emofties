5401afcb1396597515e7f7ca05a715d2e9d66bb1
Artem B <artem.bv@pm.me>
2023-02-03 13:23:58 +0200

    feat: post emofty tx to arweave logic

--- emofties-dapp-vite/src/services/ArweaveEmofties.ts
+++ emofties-dapp-vite/src/services/ArweaveEmofties.ts
@@ -1,4 +1,13 @@
 import { z } from "zod"
+import Arweave from "arweave"
+import { Tag } from "arweave/node/lib/transaction"
+import { JWKInterface } from "arweave/node/lib/wallet"
+
+export const arweave = Arweave.init({
+  host: import.meta.env.VITE_ARWEAVE_SERVER || "arweave.net",
+  port: import.meta.env.VITE_ARWEAVE_PORT || 443,
+  protocol: import.meta.env.VITE_ARWEAVE_NETWORK_PROTOCOL || "https",
+})
 
 const Emofty = z.object({
   id: z.string(),
@@ -11,10 +20,6 @@ const Emofty = z.object({
 })
 
 type Emofty = z.infer<typeof Emofty>
-type Tag = {
-  name: string
-  value: string
-}
 
 export const parseEmoftyTx = (tx: any) => {
   try {
@@ -53,5 +58,57 @@ export const createEmoftyTx = (
       { name: "Chain-Id", value: formData.chainId },
       { name: "Chain-Tx", value: formData.chainTx },
     ],
+  }
+}
+
+export const createDummyEmoftyTx = (
+  formData: any,
+  contentType: string | "text/plain",
+  version: string | "0.1"
+) => {
+  return {
+    tags: [
+      { name: "App-Name", value: "Emofties-Dapp" },
+      { name: "Protocol", value: "Emofties" },
+      { name: "Content-Type", value: contentType },
+      { name: "App-Version", value: version },
+      { name: "Unx-Time", value: 1675333392 },
+      { name: "Core-Emotion", value: "JOY" },
+      { name: "Emotion-Shade", value: "SATISFACTION" },
+      { name: "Emoji", value: "x" },
+      { name: "Chain-Id", value: "1" },
+      { name: "Chain-Tx", value: "0x" },
+    ],
+  }
+}
+
+const generateArweaveKey = async (): Promise<JWKInterface> => {
+  const key = await arweave.wallets.generate()
+  const addr = await arweave.wallets.jwkToAddress(key)
+  const tokens = arweave.ar.arToWinston("100")
+  console.log(`Key: ${key}, Addr: ${addr}, AR winstons: ${tokens}`)
+  // mint some tokens
+  await arweave.api.get(`mint/${addr}/${tokens}`)
+  await arweave.api.get("mine")
+  return key
+}
+
+const publishEmoftyTransaction = async (
+  key: JWKInterface | undefined,
+  tags: Tag[]
+) => {
+  let tx = await arweave.createTransaction(
+    {
+      data: "Hello World",
+      tags,
+    },
+    key ?? "use_wallet"
+  )
+  await arweave.transactions.sign(tx, key ?? "use_wallet")
+
+  const uploader = await arweave.transactions.getUploader(tx)
+  while (!uploader.isComplete) {
+    await uploader.uploadChunk()
+    console.log(`${uploader.pctComplete}%`)
   }
 }
